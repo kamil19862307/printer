566513a574e806dd1887ca97af394b899b8d7a58
kamil19862307 <kamil19862307@gmail.com>
2026-07-17 13:19:19 +0300

    first init, install laravel, add Printer model and migration

---
+++ src/app/Models/Printer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Printer extends Model
+{
+    /** @use HasFactory<\Database\Factories\PrinterFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image_path',
+        'price',
+        'brand',
+        'model',
+        'closed_at',
+    ];
+}

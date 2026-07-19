8f07b331c51938cf8e687e99036947a30aceef80
kamil19862307 <kamil19862307@gmail.com>
2026-07-19 15:18:27 +0300

    feat(Filament): install admin panel, create Printer entity

---
+++ src/app/Filament/Resources/PrinterResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PrinterResource\Pages;
+use App\Filament\Resources\PrinterResource\RelationManagers;
+use App\Models\Printer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PrinterResource extends Resource
+{
+    protected static ?string $model = Printer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('image_path')
+                    ->image()
+                    ->directory('printers')
+                    ->imageEditor()
+                    ->disk('public'),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->prefix('$'),
+                Forms\Components\TextInput::make('brand')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('model')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('state')
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('closed_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image_path'),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('brand')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('model')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('state')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('closed_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPrinters::route('/'),
+            'create' => Pages\CreatePrinter::route('/create'),
+            'edit' => Pages\EditPrinter::route('/{record}/edit'),
+        ];
+    }
+}

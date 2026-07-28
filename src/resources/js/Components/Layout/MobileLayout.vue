4eda09e36f8262182fdc47a4d9ec536995901b58
kamil19862307 <kamil19862307@gmail.com>
2026-07-28 09:35:29 +0300

    feat: checkout in front branch, install @vueuse/core, clsx, @heroicons/vue, swiper

---
+++ src/resources/js/Components/Layout/MobileLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <main
+        class="min-h-screen bg-slate-100 flex justify-center"
+    >
+        <div
+            class="w-full max-w-[750px] bg-white min-h-screen shadow-xl"
+        >
+            <slot />
+        </div>
+    </main>
+</template>
---
+++ src/resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MobileLayout from '@/Components/Layout/MobileLayout.vue'
+</script>
+
+<template>
+    <MobileLayout>
+
+        Тут пока будет главная страница
+
+    </MobileLayout>
+</template>
